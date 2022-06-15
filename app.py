13b2dc1613fdbf73e34e7029ae48c793d88993ad
Diogo Garcia Moura <104539978+DgiooAmoru@users.noreply.github.com>
2022-06-15 20:40:54 -0300

    feature criacao de classes (#23)

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask
+from unicodedata import name
+from flask import Flask, Response, request
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+import json
+from dotenv import load_dotenv
+import os
+load_dotenv()  # take environment variables from .env.
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB")
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
